d40fdaa74933bc86fe215fd0b77d8a920325f390
rajendra-rpavankumar <rajendra.rpavankumar@gmail.com>
2017-06-16 22:11:03 +0100

    Commit

---
+++ AFHadoopTask/PracticesDataMapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Hadoop.MapReduce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFTask.Mapper
+{
+
+    public class PracticesDataMapper : MapperBase
+    {
+
+        public override void Map(string inputLine, MapperContext context)
+        {
+           
+
+                context.EmitKeyValue("LONDON", "LONDON");
+            
+        }
+    }
+}
---
+++ AFHadoopTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Hadoop.MapReduce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFHadoopTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HadoopJobConfiguration practice_jobConfig = new HadoopJobConfiguration()
+            {
+                InputPath = "Input/AFTask",
+                OutputFolder = "Output/AFTaskPractices",
+            };
+        }
+    }
+}
